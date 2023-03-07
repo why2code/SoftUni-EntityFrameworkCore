0d496959cf6d6c894f4929e8600d9e349c6bf703
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-07 13:57:54 +0200

    Advanced Querying - BookShop DB skeleton and 02. Age Restriction

---
+++ Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine().ToLower();
+            string result = GetBooksByAgeRestriction(db, command);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int groupId = 0; //for Minor
+            if (command == "teen") //Teen
+            {
+                groupId = 1;
+            }
+            else if (command == "adult") //Adult
+            {
+                groupId = 2;
+            }
+
+            var books = context.Books
+                .Where(b => (int)b.AgeRestriction == groupId)
+                .Select(b => new
+                {
+                   title = b.Title
+                })
+                .OrderBy(b => b.title)
+                .ToArray();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
