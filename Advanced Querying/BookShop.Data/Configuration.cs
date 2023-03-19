bd7599676a1b65a85c9acf283e539f9aead99fba
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-19 11:09:10 +0200

    JSON AND XML Exercise structure + JSON problems 1-8 solution

---
+++ Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-C3LK4PO\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ JSON/ProductShop/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-C3LK4PO\SQLEXPRESS;Database=ProductShop;Integrated Security=True;Encrypt=False;";
+    }
+}
