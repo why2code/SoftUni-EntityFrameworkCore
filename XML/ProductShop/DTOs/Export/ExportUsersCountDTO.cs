7e0f476c5ec9311347d5cecf87898aab51f17e1d
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-21 21:23:27 +0200

    Problems 06 and 07

---
+++ XML/ProductShop/DTOs/Export/ExportUsersCountDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Export
+{
+    [XmlType("Users")]
+    public class ExportUsersCountDTO
+    {
+
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+
+        [XmlElement("users")]
+        public ExportUsersIntoUsersCountDTO Users { get; set; }
+
+    }
+}
---
+++ XML/ProductShop/DTOs/Export/ExportUsersIntoUsersCountDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Export
+{
+    [XmlType("User")]
+    public class ExportUsersIntoUsersCountDTO
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; } = null!;
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; } = null!;
+
+        [XmlElement("age")]
+        public string Age { get; set; }
+
+        [XmlElement("SoldProducts")]
+        public ExportSoldProductsDTO SoldProducts { get; set; }
+    }
+}
