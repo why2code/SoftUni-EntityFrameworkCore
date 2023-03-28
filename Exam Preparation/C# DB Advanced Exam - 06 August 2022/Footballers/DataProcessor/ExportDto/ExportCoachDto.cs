6cc6df8bec1b785818df1625f0a84cf98accde23
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-29 00:17:13 +0300

    Exam Preparation C# DB Advanced Exam - 06 August 2022 / Problem 03. Data Export - 12/25 points

---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ExportDto/ExportCoachDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Footballers.Data.Models;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Coach")]
+    public class ExportCoachDto
+    {
+        [XmlAttribute("FootballersCount")]
+        public int FootballersCount { get; set; }
+
+        [XmlElement("CoachName")]
+        public string CoachName { get; set; } = null!;
+
+        [XmlArray("Footballers")]
+        public ExportFootballerDto[] Footballers { get; set; }
+    }
+}
--- Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/Serializer.cs
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/Serializer.cs
@@ -1,17 +1,67 @@
-﻿namespace Footballers.DataProcessor
+﻿using System.Globalization;
+using Footballers.DataProcessor.ExportDto;
+using Footballers.Utilities;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Footballers.DataProcessor
 {
     using Data;
+    using Footballers.Data.Models.Enums;
 
     public class Serializer
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
-            throw new NotImplementedException();
+            XmlHelper xmlHelper = new XmlHelper();
+            var coachesDb = context.Coaches
+                .Where(c => c.Footballers.Count >= 1)
+                .Select(c => new ExportCoachDto()
+                {
+                    FootballersCount = c.Footballers.Count,
+                    CoachName = c.Name,
+                    Footballers = c.Footballers.Select(f => new ExportFootballerDto()
+                        {
+                            Name = f.Name,
+                            PositionType = f.PositionType.ToString()
+                        })
+                        .OrderBy(f => f.Name)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.Footballers.Length)
+                .ThenBy(c => c.CoachName)
+                .ToArray();
+
+            return xmlHelper.Serialize(coachesDb, "Coaches");
         }
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var teams = context.Teams
+                .Include(tf => tf.TeamsFootballers)
+                .Where(t => t.TeamsFootballers.Any(f => f.Footballer.ContractStartDate >= date))
+                .ToArray()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Footballers = t.TeamsFootballers.Select(tfb => new
+                        {
+                            FootballerName = tfb.Footballer.Name,
+                            ContractStartDate = tfb.Footballer.ContractStartDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                            ContractEndDate = tfb.Footballer.ContractEndDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                            BestSkillType = tfb.Footballer.BestSkillType.ToString(),
+                            PositionType = tfb.Footballer.PositionType.ToString()
+                        }).OrderByDescending(t =>
+                            DateTime.ParseExact(t.ContractEndDate, "MM/dd/yyyy", CultureInfo.InvariantCulture))
+                        .ThenBy(n => n.FootballerName)
+                        .ToArray()
+                })
+                .OrderByDescending(m => m.Footballers.Count())
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(teams, Formatting.Indented);
         }
     }
 }
