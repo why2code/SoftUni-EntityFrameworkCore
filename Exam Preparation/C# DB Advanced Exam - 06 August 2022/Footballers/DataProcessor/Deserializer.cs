d3136a1c2184617ca5da383f3796fa7796812bb2
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-28 23:12:51 +0300

    Exam Preparation C# DB Advanced Exam - 06 August 2022 / Problem 02. Data Import

--- Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/Deserializer.cs
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/Deserializer.cs
@@ -1,4 +1,11 @@
-﻿using Footballers.Utilities;
+﻿using System.Globalization;
+using Footballers.Data.Models;
+using Footballers.Data.Models.Enums;
+using Footballers.DataProcessor.ImportDto;
+using Footballers.Utilities;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Footballers.DataProcessor
 {
@@ -19,12 +26,137 @@ namespace Footballers.DataProcessor
 
         public static string ImportCoaches(FootballersContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportCoachDto[] coachesDtos = xmlHelper.Deserialize<ImportCoachDto[]>(xmlString, "Coaches");
+
+            ICollection<Coach> validCoaches = new HashSet<Coach>();
+
+            foreach (ImportCoachDto coachDto in coachesDtos)
+            {
+                if (!IsValid(coachDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+
+
+                //Validation for each footballer
+                ICollection<Footballer> validFootballers = new HashSet<Footballer>();
+                foreach (ImportFootballerDto footballer in coachDto.Footballers)
+                {
+                    if (!IsValid(footballer))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    DateTime startD = DateTime.ParseExact(footballer.ContractStartDate, "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture);
+                    DateTime EndD = DateTime.ParseExact(footballer.ContractEndDate, "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture);
+
+                    //The test in Expected results does not seem to have check for Date!
+                    //Ref. Coach Gerardo Seoane should be imported with 1 player, not 2 (in expected)
+                    if (EndD < startD)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Footballer currentFootballer = new Footballer()
+                    {
+                        Name = footballer.Name,
+                        ContractStartDate = startD,
+                        ContractEndDate = EndD,
+                        BestSkillType = (BestSkillType)footballer.BestSkillType,
+                        PositionType = (PositionType)footballer.PositionType
+                    };
+
+                    validFootballers.Add(currentFootballer);
+                }
+
+                Coach currentValidCoach = new Coach()
+                {
+                    Name = coachDto.Name,
+                    Nationality = coachDto.Nationality,
+                    Footballers = validFootballers
+                };
+
+
+                validCoaches.Add(currentValidCoach);
+                sb.AppendLine(string.Format(SuccessfullyImportedCoach, currentValidCoach.Name,
+                    validFootballers.Count));
+            }
+
+            context.Coaches.AddRange(validCoaches);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportTeams(FootballersContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            ICollection<Team> validTeamsOutput = new HashSet<Team>();
+
+            ImportTeamsDto[] teamsDtos = JsonConvert.DeserializeObject<ImportTeamsDto[]>(jsonString);
+
+            foreach (ImportTeamsDto team in teamsDtos)
+            {
+                if (!IsValid(team))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (team.Trophies == 0)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Team currValidTeam = new Team()
+                {
+                    Name = team.Name,
+                    Nationality = team.Nationality,
+                    Trophies = team.Trophies
+                };
+
+
+                //Moving into validating the Footballer IDs provided with the json import:
+                ICollection<Footballer> validFootballers = new HashSet<Footballer>();
+                ICollection<int> dbFootballers = context.Footballers
+                    .AsNoTracking()
+                    .Select(f => f.Id)
+                    .ToArray();
+                
+
+                foreach (var footballerId in team.Footballers.Distinct())
+                {
+                    if (!dbFootballers.Contains(footballerId))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    TeamFootballer tmf = new TeamFootballer()
+                    {
+                        Team = currValidTeam,
+                        FootballerId = footballerId
+                    };
+
+                    currValidTeam.TeamsFootballers.Add(tmf);
+                }
+
+                validTeamsOutput.Add(currValidTeam);
+                sb.AppendLine(string.Format(SuccessfullyImportedTeam, currValidTeam.Name,
+                    currValidTeam.TeamsFootballers.Count));
+            }
+
+            context.Teams.AddRange(validTeamsOutput);
+            context.SaveChanges();
+            return sb.ToString().Trim();
         }
 
         private static bool IsValid(object dto)
---
+++ Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportCoachDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class ImportCoachDto
+    {
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("Nationality")]
+        [Required]
+        public string Nationality { get; set; } = null!;
+
+        [XmlArray("Footballers")]
+        public ImportFootballerDto[] Footballers { get; set; }
+    }
+}
