1f6c574793fabc338b5f3702c5cef76970d45fc5
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-29 20:46:32 +0300

    C# DB Advanced Exam - 04 Dec 2021 / Skeleton + Problem 01. Model Definition

---
+++ Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Play.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        public Play()
+        {
+            this.Casts = new HashSet<Cast>();
+            this.Tickets = new HashSet<Ticket>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public float Rating { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [MaxLength(700)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string Screenwriter { get; set; } = null!;
+
+        public virtual ICollection<Cast> Casts { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+
+    }
+}
---
+++ Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/TheatreContext.cs
@@ -0,0 +1,49 @@
+﻿
+using Theatre.Data.Models;
+// ReSharper disable IdentifierTypo
+
+namespace Theatre.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    // ReSharper disable once IdentifierTypo
+    public class TheatreContext : DbContext
+    {
+        public TheatreContext() 
+        {
+        }
+
+        public TheatreContext(DbContextOptions options)
+        : base(options) 
+        { 
+        }
+
+        public DbSet<Cast> Casts { get; set; } = null!;
+
+        public DbSet<Play> Plays { get; set; } = null!;
+
+        public DbSet<Models.Theatre> Theatres { get; set; } = null!;
+
+        public DbSet<Ticket> Tickets { get; set; } = null!;
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Ticket>(e =>
+            {
+                e.HasKey(pk => new {pk.PlayId, pk.TheatreId});
+            });
+        }
+    }
+}
