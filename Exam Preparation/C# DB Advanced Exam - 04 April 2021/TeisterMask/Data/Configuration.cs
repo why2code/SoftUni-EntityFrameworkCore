fce7667ca47353d56f0c38b1e0fd9f50ec73a365
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2023-03-30 20:38:59 +0300

    C# DB Advanced Exam - 04 April 2021 / Skeleton, Problem Description and 01. Model Definition

---
+++ Exam Preparation/C# DB Advanced Exam - 04 April 2021/TeisterMask/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-C3LK4PO\SQLEXPRESS;Database=TeisterMaskExam;Integrated Security=True;Encrypt=False";
+    }
+}
---
+++ Exam Preparation/C# DB Advanced Exam - 04 April 2021/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+
+        public Employee()
+        {
+            this.EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Phone { get; set; } = null!;
+
+        public virtual ICollection<EmployeeTask> EmployeesTasks { get;}
+
+    }
+}
